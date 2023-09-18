455f2139e040e70acf7650bd1e4e61a363eaa7e7
iamkrati22 <iamkrati22@gmail.com>
2023-09-18 18:45:55 +0530

    Added MongoDb connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
-import TextEditor from './TextEditor';
+import "./styles.css";
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
+import { v4 as uuidV4 } from 'uuid'
 
 function App() {
   return (
-   <TextEditor/>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" exact element={
+          <Navigate to={`/documents/${uuidV4()}`} />
+        }>  
+        </Route>
+
+        <Route path="/documents/:id" element={
+          <TextEditor />
+        }>
+        </Route>
+
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,49 @@
-const io = require('socket.io')(3001, {
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { Server } = require('socket.io')
+const Document = require('./Document')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    res.write('Hello World!'); //write a response
+    res.end(); //end the response
+  }).listen(process.env.PORT, () => {
+   console.log(`server start at port ${process.env.PORT}`);
+  });
+
+mongoose.connect(`mongodb+srv://iamkrati22:Krati220502@cluster0.gaoifgl.mongodb.net/?retryWrites=true&w=majority`);
+
+const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        methods: ["GET", "POST"]
+        // origin: 'http://localhost:3000',
+        origin: 'https://localhost:3000',
+        methods: ['GET', 'POST'],
     },
 })
 
+const defaultValue = ""
 
 io.on("connection", socket => {
-    console.log("connected");
+    socket.on("get-document", async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        socket.emit("load-document", document.data)
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        })
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data })
+        })
+    })
 })
+
+async function findOrCreateDocument(id) {
+    if (id == null) return
+
+    const document  = await Document.findById(id)
+    if (document) return document
+
+    return await Document.create({_id: id, data: defaultValue})
+}
